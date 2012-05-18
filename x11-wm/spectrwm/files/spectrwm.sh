12574898a7813b286396eec49b17a3aaf357c843
Iegor <rmtdev@gmail.com>
2012-05-18 13:47:03 +0300

    added 0ad game and updated spectrewm sh startup script.

--- x11-wm/spectrwm/files/spectrwm.sh
+++ x11-wm/spectrwm/files/spectrwm.sh
@@ -5,21 +5,14 @@ xsetbg -onroot -fullscreen -center -shrink -border black /home/rainman/.scrotwm/
 
 sleep .2
 
-#xrandr --dpi 96
-
 # auto launch apps
-# /usr/libexec/gnome-settings-daemon &
-# /usr/bin/gnome-power-manager &
-# /usr/bin/nm-applet &
-# /usr/bin/gkrellm &
 /usr/bin/xscreensaver -no-splash &
 
 # set xkb options
 setxkbmap -option grp:shift_caps_toggle
 setxkbmap -model pc104 -layout us,ru -variant .winkeys
 if [ "$?" = "0" ]; then
-        notify-send "keyboard options xkb are set"
+        notify-send "xkb is set"
 fi
 
-# launch our wm
 exec spectrwm
